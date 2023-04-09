502a1349efffabd6260f67e395ee52e2371386f3
unknown <vdnam.ute@gmail.com>
2023-04-09 13:43:47 +0700

    Update

--- install_lib.sh
+++ install_lib.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 gnome-terminal --working-directory=/home/ubuntu/Desktop/ -x bash -c " \
-    sudo apt install git;sleep 2; \
-    git --version; \
-    git clone https://github.com/vannamlord/dws_record.git; sleep 2; \
-    sudo apt install speedtest-cli;sleep 2;apt update; \
-    sudo gzip -k9 /usr/lib/python3/dist-packages/speedtest.py; sleep 2; \
-    sudo wget https://raw.githubusercontent.com/sivel/speedtest-cli/v2.1.3/speedtest.py -O /usr/lib/python3/dist-packages/speedtest.py;sleep 2; \
-    cp -i /home/ubuntu/Desktop/dws_record/run.desktop ~/.config/autostart;sleep 2; \
+    sudo apt install git;sleep 1;git --version;sleep 1;\
+    git clone https://github.com/vannamlord/dws_record.git; sleep 1; \
+    sudo apt install python3-tk;sleep 1; \
+    sudo apt install speedtest-cli;sleep 1; \
+    sudo gzip -k9 /usr/lib/python3/dist-packages/speedtest.py; sleep 1; \
+    sudo wget https://raw.githubusercontent.com/sivel/speedtest-cli/v2.1.3/speedtest.py \
+    -O /usr/lib/python3/dist-packages/speedtest.py;sleep 1; \
+    cp -i /home/ubuntu/Desktop/dws_record/run.desktop ~/.config/autostart;sleep 1; \
     shutdown -r now"
