7f7c69f9709dca271bb667526ead1bc6adcf9e8d
unknown <vdnam.ute@gmail.com>
2023-04-12 13:13:19 +0700

    Update

--- install_lib.sh
+++ install_lib.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 gnome-terminal --working-directory=/home/ubuntu/Desktop/ -x bash -c " \
-    sudo apt install git;sleep 1;git --version;sleep 1;\
-    git clone https://github.com/vannamlord/dws_record.git; sleep 1; \
     sudo apt install python3-tk;sleep 1; \
     sudo apt install speedtest-cli;sleep 1; \
     sudo gzip -k9 /usr/lib/python3/dist-packages/speedtest.py; sleep 1; \
